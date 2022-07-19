2ab836ac163c1b3a22b73b9cb7fd5a0ad45e45ea
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-07-19 16:38:20 -0500

    made navbar into a modal, fixed spacing issues

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,6 +11,14 @@ function Dashboard(){
         <div className="item-box">Projects</div>
         <div className="item-box">Contact</div>
         <div className="item-box">About</div>
+        <div className="item-box">Home</div>
+        <div className="item-box">Projects</div>
+        <div className="item-box">Contact</div>
+        <div className="item-box">About</div>
+        <div className="item-box">Home</div>
+        <div className="item-box">Projects</div>
+        <div className="item-box">Contact</div>
+        <div className="item-box">About</div>
         
     </article>
 }
