741718dfa6d65a3bc1cd162a7b39e1f779e0aba3
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-07-19 14:18:05 -0500

    Added main page

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import './Dashboard.css'
+
+
+function Dashboard(){
+    return <article className='items-container width-container'>
+        <div className="item-box">Home</div>
+        <div className="item-box">Projects</div>
+        <div className="item-box">Contact</div>
+        <div className="item-box">About</div>
+    </article>
+}
+
+export {Dashboard}
