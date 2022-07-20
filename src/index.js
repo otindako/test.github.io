0dbee3f5ffc5e8758df8dbd937963fec6450029e
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-07-20 00:12:26 -0500

    Converted SPA to PWA

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
 
 if ("serviceWorker" in navigator) {
   // register service worker
-  navigator.serviceWorker.register('service-worker.js');
+  navigator.serviceWorker.register('https://test.react-test-dgm.pages.dev/service-worker.js');
 }
 
 loadTheme()
