63f6e039b3cef93c46738c211f80e76be84ba286
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-07-19 20:25:05 -0500

    Removed home screen boxes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./index.css"
-import './App.css';
 import Main from './pages/main.js'
 
 function App() {
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,12 +5,7 @@ import './Home.css'
 class Home extends Component {
     render() {
       return (
-        <article className='items-container width-container'>
-        <div className="item-box">Home</div>
-        <div className="item-box">Projects</div>
-        <div className="item-box">Contact</div>
-        <div className="item-box">About</div>
-      </article>
+        <></>
       );
     }
 }
